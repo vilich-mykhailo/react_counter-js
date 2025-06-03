8782f03c772b370370196350236a8179048de4b8
Mykhailo Vilich <vilich96m@gmail.com>
2025-06-03 11:23:08 +0200

    addd

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,24 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(prev => prev + 1);
   };
 
   const add100 = () => {
-    // write code here
+    setCount(prev => prev + 100);
   };
 
-  // DON'T change the code below
   const increase = () => {
-    if (count % 5 === 0) {
-      add100();
-    }
+    const newCount = count + 1;
 
-    addOne();
+    setCount(newCount);
+
+    if (newCount % 5 === 0) {
+      setCount(prev => prev + 100);
+    }
   };
 
   return (
